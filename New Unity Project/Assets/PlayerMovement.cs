89b7a12e072e98c365e5b5f48fb044f8498290e2
justsomeperson <59874159+justsomeperson@users.noreply.github.com>
2021-04-24 23:14:14 +0200

    First push
    Player movement & first person camera view

---
+++ New Unity Project/Assets/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public int ConstantSpeed = 0;
+    public int MovementSpeed = 2000;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, ConstantSpeed * Time.deltaTime);
+
+        if (Input.GetKey("w"))
+        {
+            //rigidbody.velocity = Vector3.zero;
+            //rigidbody.angularVelocity = Vector3.zero;
+            rb.AddForce(0, 0, MovementSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            //rigidbody.velocity = Vector3.zero;
+            //rigidbody.angularVelocity = Vector3.zero;
+            rb.AddForce(-MovementSpeed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            //rigidbody.velocity = Vector3.zero;
+            //rigidbody.angularVelocity = Vector3.zero;
+            rb.AddForce(0, 0, -MovementSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            //rigidbody.velocity = Vector3.zero;
+            //rigidbody.angularVelocity = Vector3.zero;
+            rb.AddForce(MovementSpeed * Time.deltaTime, 0, 0);
+        }
+    }
+}
